c97ad3cc6500fea590f71d8274fb1bd0f127bf14
kraksy <kraksy8@gmail.com>
2023-01-28 21:55:24 +0100

    Initial commit

---
+++ src/main/java/io/github/kraksy/teamc/TeaMC.java
@@ -0,0 +1,17 @@
+package io.github.kraksy.teamc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TeaMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
