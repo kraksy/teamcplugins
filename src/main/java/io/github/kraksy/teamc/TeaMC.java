4b092b66de5b9edbc1267edececca3dc7ac55e32
kraksy <kraksy8@gmail.com>
2023-01-30 18:49:35 +0100

    meh

--- src/main/java/io/github/kraksy/teamc/TeaMC.java
+++ src/main/java/io/github/kraksy/teamc/TeaMC.java
@@ -1,6 +1,6 @@
 package io.github.kraksy.teamc;
 
-import io.github.kraksy.teamc.eventlisteners.onPlayerJoinListener;
+import io.github.kraksy.teamc.eventlisteners.onPlayerJoinLeaveListener;
 import io.github.kraksy.teamc.eventlisteners.testingEvents;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,7 +12,7 @@ public final class TeaMC extends JavaPlugin implements Listener {
 
         System.out.println("its working!");
 
-        getServer().getPluginManager().registerEvents( new onPlayerJoinListener(), this);
+        getServer().getPluginManager().registerEvents( new onPlayerJoinLeaveListener(), this);
         getServer().getPluginManager().registerEvents( new testingEvents(), this);
 
 
--- src/main/java/io/github/kraksy/teamc/eventlisteners/onPlayerJoinLeaveListener.java
+++ src/main/java/io/github/kraksy/teamc/eventlisteners/onPlayerJoinLeaveListener.java
@@ -1,23 +1,25 @@
 package io.github.kraksy.teamc.eventlisteners;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
-public class onPlayerJoinListener implements Listener {
+public class onPlayerJoinLeaveListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
 
         Player player = event.getPlayer();
-        System.out.println("somebody joined");
+        event.setJoinMessage(ChatColor.DARK_AQUA + player.getDisplayName() + ChatColor.DARK_GRAY + "joined");
         event.setJoinMessage("damn hello");
 
         int time = player.getStatistic(Statistic.PLAY_ONE_MINUTE);
-        int hoursTime = time / 20 * 60 * 60;
-        int minTime = time / 20 * 60;
+        int hoursTime = time / 20 / 60 / 60;
+        int minTime = time / 20 / 60;
         int secTime = time / 20;
         String timePlayedhours = String.valueOf(hoursTime);
         String timePlayedmin = String.valueOf(minTime);
@@ -25,6 +27,15 @@ public class onPlayerJoinListener implements Listener {
 
         //time in tick
         player.sendMessage("you were playing here for >>", timePlayedhours , "hours", timePlayedmin , "min", timePlayedsec , "sec");
+
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event){
+
+        Player player = event.getPlayer();
+
+        event.setQuitMessage(ChatColor.DARK_AQUA + player.getDisplayName() + ChatColor.DARK_GRAY + "leaved");
 
     }
 }
