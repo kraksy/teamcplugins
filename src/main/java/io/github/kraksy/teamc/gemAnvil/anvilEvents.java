185744610ceea1cbf455ce67506ce2c99edd161f
kraksy <kraksy8@gmail.com>
2023-02-04 17:59:33 +0100

    meh

--- src/main/java/io/github/kraksy/teamc/gemAnvil/anvilEvents.java
+++ src/main/java/io/github/kraksy/teamc/gemAnvil/anvilEvents.java
@@ -1,24 +1,17 @@
 package io.github.kraksy.teamc.gemAnvil;
 
 import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Objects;
-
-public class anvilEvents implements Listener , CommandExecutor {
+public class anvilEvents implements Listener  {
 
     public int arsenalSlot = 14;
     public int gemSlot = 12;
     public String gemName = "ruby";
-    
-
 
     @EventHandler
     public void clickGui(InventoryClickEvent event) {
@@ -28,37 +21,30 @@ public class anvilEvents implements Listener , CommandExecutor {
 
         ItemStack gem = new ItemStack(Material.PLAYER_HEAD);
         ItemStack arsenal = new ItemStack(Material.WOODEN_SWORD);
+        ItemStack glass = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
+
 
         Player player = (Player) event.getWhoClicked();
         String inv = event.getView().getTitle();
 
-        if (Objects.requireNonNull(event.getCurrentItem()).getType() == Material.GRAY_STAINED_GLASS_PANE) {
-            event.setCancelled(true);
-        }
-        
-
         if (inv.equalsIgnoreCase("gemAnvil")){
+
+            ItemStack clickItem = event.getCurrentItem();
+
+            assert clickItem != null;
+            if (clickItem.equals(glass)) {
+                event.setCancelled(true);
+            }
 
             assert arsenalSpot != null;
             if (arsenalSpot.equals(arsenal)) {
-
                 player.sendMessage("its working a!");
-
             }
 
             assert gemSpot != null;
             if (gemSpot.equals(gem)) {
-
                 player.sendMessage("its working b!");
-
             }
-
         }
-
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
     }
 }
